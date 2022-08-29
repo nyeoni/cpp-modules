990437514551f78914536671eb546c6c2c8dbbdf
Chloekkk <skdusdl8804@gmail.com>
2022-08-29 13:38:22 +0900

    feat: iostream exception handling

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -13,19 +13,23 @@ int main() {
   PhoneBook phoneBook;
   std::string command;
 
-  while (true) {
-    std::cout << "\033[1;36mSELECT COMMAND ADD | SEARCH | EXIT : \033[0m";
-    std::getline(std::cin, command);
+  std::cin.exceptions(std::ios::eofbit | std::ios::failbit | std::ios::badbit);
+  try {
+    while (true) {
+      std::cout << "\033[1;36mSELECT COMMAND ADD | SEARCH | EXIT : \033[0m";
+      std::getline(std::cin, command);
 
-    if (command == "ADD") {
-      phoneBook.addContact();
-    } else if (command == "SEARCH") {
-      phoneBook.searchContact();
-    } else if (command == "EXIT") {
-      break;
-    } else {
-      std::cout << "Error: Wrong Command." << std::endl;
+      if (command == "ADD")
+        phoneBook.addContact();
+      else if (command == "SEARCH")
+        phoneBook.searchContact();
+      else if (command == "EXIT")
+        break;
+      else
+        std::cout << "Error: Wrong Command." << std::endl;
     }
+  } catch (std::exception &e) {
+    if (!std::cin.eof()) std::cout << "Error: " << e.what() << std::endl;
   }
   return 0;
 }
