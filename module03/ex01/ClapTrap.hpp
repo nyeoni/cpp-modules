1100f7b30889a118feb4fdccf767b9a653159847
Chloekkk <skdusdl8804@gmail.com>
2022-11-07 23:52:52 +0900

    feat: Complete module03/ex01

---
+++ module03/ex01/ClapTrap.hpp
@@ -0,0 +1,53 @@
+/*
+ * File: ClapTrap.hpp
+ * Project: ex00
+ * Created Date: 2022/11/06
+ * Author: nkim
+ * Copyright (c) 2022 nkim
+ */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+enum CT_STATUS {
+  DEAD,
+  NO_EP,
+  ALIVE
+};
+
+class ClapTrap {
+ protected:
+  std::string actor_;
+  std::string name_;
+  unsigned int hp_;
+  unsigned int ep_;
+  unsigned int damage_;
+
+ public:
+  ClapTrap(void);
+  ClapTrap(const std::string &name);
+  ClapTrap(const ClapTrap &copy);
+
+  ClapTrap &operator=(const ClapTrap &src);
+
+  const std::string &getName() const;
+  void setName(const std::string &name);
+  unsigned int getHp() const;
+  void setHp(unsigned int hp);
+  unsigned int getEp() const;
+  void setEp(unsigned int ep);
+  unsigned int getDamage() const;
+  void setDamage(unsigned int damage);
+
+  virtual void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+  void printStatus();
+  CT_STATUS checkStatus();
+
+  ~ClapTrap();
+};
+
+#endif //CLAPTRAP_HPP
---
+++ module03/ex01/ScavTrap.cpp
@@ -0,0 +1,50 @@
+/*
+ * File: ScavTrap.cpp
+ * Project: ex01
+ * Created Date: 2022/11/07
+ * Author: nkim
+ * Copyright (c) 2022 nkim
+ */
+
+#include "ScavTrap.hpp"
+#include <iostream>
+
+// Constructor & Destructor
+ScavTrap::ScavTrap(void) : ClapTrap() {
+  this->actor_ = "ScavTrap";
+  this->hp_ = 100;
+  this->ep_ = 50;
+  this->damage_ = 20;
+  std::cout << "[ScavTrap] " << "Default constructor called" << std::endl;
+}
+ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name) {
+  this->actor_ = "ScavTrap";
+  this->hp_ = 100;
+  this->ep_ = 50;
+  this->damage_ = 20;
+  std::cout << "[ScavTrap] " << "Constructor(string name_) called" << std::endl;
+}
+ScavTrap::ScavTrap(const ScavTrap &copy) : ClapTrap(copy) {
+  *this = copy;
+  std::cout << "[ScavTrap] " << "Copy constructor called" << std::endl;
+}
+ScavTrap::~ScavTrap() {
+  std::cout << "[ScavTrap] " << "Destructor called" << std::endl;
+}
+
+// Operator Overload
+ScavTrap &ScavTrap::operator=(const ScavTrap &src) {
+  this->name_ = src.getName();
+  this->hp_ = src.getHp();
+  this->ep_ = src.getEp();
+  this->damage_ = src.getDamage();
+  return (*this);
+}
+
+// Methods
+void ScavTrap::attack(const std::string &target) {
+  ClapTrap::attack(target);
+}
+void ScavTrap::guardGate() {
+  std::cout << "ScavTrap is now in Gate keeper mode" << std::endl;
+}
