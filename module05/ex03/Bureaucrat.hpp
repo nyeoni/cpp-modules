508045663646033849fba65d2054c8e12c174fe2
Chloekkk <skdusdl8804@gmail.com>
2022-11-12 21:37:04 +0900

    feat: Init module05/ex03

---
+++ module05/ex03/Bureaucrat.hpp
@@ -0,0 +1,47 @@
+/*
+ * File: Bureaucrat.hpp
+ * Project: ex00
+ * Created Date: 2022/11/11
+ * Author: nkim
+ * Copyright (c) 2022 nkim
+ */
+
+#ifndef BUREAUCRAT_HPP_
+#define BUREAUCRAT_HPP_
+
+#include <string>
+
+class Form;
+class Bureaucrat {
+ private:
+  const std::string name_;
+  int grade_;
+ public:
+  Bureaucrat();
+  Bureaucrat(const std::string &name, int grade);
+  Bureaucrat(const Bureaucrat &copy);
+  ~Bureaucrat();
+
+  Bureaucrat &operator=(const Bureaucrat &src);
+
+  const std::string &getName() const;
+  int getGrade() const;
+
+  void incrementGrade();
+  void decrementGrade();
+  void signForm(Form &form);
+  void executeForm(Form const &form);
+
+  class GradeTooHighException : public std::exception {
+   public:
+    virtual const char *what() const throw();
+  };
+  class GradeTooLowException : public std::exception {
+   public:
+    virtual const char *what() const throw();
+  };
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif //BUREAUCRAT_HPP_
